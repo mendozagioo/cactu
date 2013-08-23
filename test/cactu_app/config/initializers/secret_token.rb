e9d7655769ea77281ad2954871c050d15375959c
Giovanni Mendoza <mendozagioo@gmail.com>
2013-08-23 18:06:36 -0500

    Adding a Rails project to check the gem on fly

---
+++ test/cactu_app/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+CactuApp::Application.config.secret_key_base = '17e10fc6ad535863c3331d5f38d0e59f76846f6d69c38d5386cfd5ecd531e13cfc2f48cc37eb472f2604ea237c7fecdfac3ae779caffa817f4f924afcf4d406c'
---
+++ test/cactu_app/config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+CactuApp::Application.config.session_store :cookie_store, key: '_cactu_app_session'
