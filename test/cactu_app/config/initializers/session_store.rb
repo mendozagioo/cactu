77ca1666a0726c434f0cefedabd2be1c43557b25
Giovanni Mendoza <mendozagioo@gmail.com>
2014-01-08 13:10:55 -0600

    No tests in gem

--- test/cactu_app/config/initializers/session_store.rb
+++
@@ -1,3 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-CactuApp::Application.config.session_store :cookie_store, key: '_cactu_app_session'
--- test/cactu_app/config/routes.rb
+++
@@ -1,6 +0,0 @@
-CactuApp::Application.routes.draw do
-  root to: 'home#index'
-  get 'usage/started', :to => 'usage#started'
-  get 'usage/base', :to => 'usage#base'
-  get 'usage/components', :to => 'usage#components'
-end
