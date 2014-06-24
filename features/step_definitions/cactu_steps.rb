13d8f7fb9f3fddc0121c8f2ba5d75d13c9e8a7ea
Giovanni <mendozagioo@gmail.com>
2014-06-23 23:30:48 -0500

    typo fixed and a test added

--- features/step_definitions/cactu_steps.rb
+++ features/step_definitions/cactu_steps.rb
@@ -1,24 +1,24 @@
-Given /^bourbon is already installed$/ do
-  install_bourbon
+Given /^cactu is already installed$/ do
+  install_cactu
 end
 
-Given /^I install bourbon to "([^"]*)"$/ do |path|
+Given /^I install cactu to "([^"]*)"$/ do |path|
 end
 
 Then /^the sass directories(?: with "([^"]+)" prefix)? should have been generated$/ do |prefix|
   sass_directories = ["addons", "css3", "functions"]
-  sass_directories.map!{ |directory| bourbon_path(prefix, directory) }
+  sass_directories.map!{ |directory| cactu_path(prefix, directory) }
   check_directory_presence(sass_directories, true)
 end
 
-Then /^the master bourbon partial should have been generated(?: within "([^"]+)" directory)?$/ do |prefix|
-  check_file_presence([bourbon_path(prefix, '_bourbon.scss')], true)
+Then /^the master cactu partial should have been generated(?: within "([^"]+)" directory)?$/ do |prefix|
+  check_file_presence([cactu_path(prefix, '_cactu.scss')], true)
 end
 
-Then /^bourbon should not have been generated$/ do
-  check_directory_presence(['bourbon'], false)
+Then /^cactu should not have been generated$/ do
+  check_directory_presence(['cactu'], false)
 end
 
-Then /^the output should contain the current version of Bourbon$/ do
-  step %(the output should contain exactly "Bourbon #{Bourbon::VERSION}\n")
+Then /^the output should contain the current version of Cactu$/ do
+  step %(the output should contain exactly "Cactu #{Bourbon::VERSION}\n")
 end
