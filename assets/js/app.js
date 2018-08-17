8763246ef2db5e4c4a4f927a2073b8b8fc2c1018
Giovanni Mendoza <mendozagioo@gmail.com>
2018-08-17 17:32:43 -0500

    Updating the little JS interactions

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,77 +1,107 @@
-$(document).ready(function() {
-  // Right navigation
-  //
-  $('.section-nav a').on('click', function(e) {
-    e.preventDefault();
-    var scrollTo = $($(this).attr('href')).offset().top - 70;
-    
-    $('html, body').animate({
-      scrollTop: scrollTo
-    }, 300);
-  });
+/*
+** A little interaction with some Cactu's componenets
+** this components is just a way for use them
+** ------------------------------------------------------
+*/
 
-  // None links
-  //
-  $('[href="#"]').on('click', function(e) {
-    e.preventDefault();
-  });
+var Cactu = {
+  init: function() {
+    // Global interactions
+    this.noLinks();
+    this.rightNavigation();
+    // Componenets
+    this.collapse();
+    this.dropDowns();
+    this.modals();
+    this.navbars();
+  },
 
-  // Dropdowns
-  //
-  $('.toggle').on('click', function() {
-    $(this).parent().toggleClass('dropped');
-  });
-
-  // Modals
-  //
-  function isOverflowing() {
-    var rect = $('html')[0].getBoundingClientRect();
-    return rect.left + rect.right < window.innerWidth;
-  }
-  var overflowing = isOverflowing();
-
-  $(window).on('resize', function() {
-    overflowing = isOverflowing();
-  });
-
-  $('.modal-launcher').on('click', function() {
-    var modalTarget = $(this).data('target');
-    $(modalTarget).addClass('active');
-    $('html').addClass('clipped').css({
-      'padding-right': overflowing ? '15px' : ''
+  noLinks: function() {
+    $('[href="#"]').on('click', function (e) {
+      e.preventDefault();
     });
-  });
-  $('.modal, .modal-center, .modal .close, .modal-center .close').on('click', function() {
-    $('.modal, .modal-center').removeClass('active');
-    setTimeout(function() {
-      $('html').removeAttr('style');
-      $('html').removeClass('clipped');
-    }, 200);
-  });
-  $('.modal-content').on('click', function(e) {
-    e.stopPropagation();
-  });
+  },
 
-  // Collapse component
-  //
-  $('#basic-collapse > button').on('click', function() {
-    $('#basic-collapse > .collapse-container').toggleClass('collapsed');
-  });
+  rightNavigation: function() {
+    $('.section-nav a').on('click', function (e) {
+      e.preventDefault();
+      var scrollTo = $($(this).attr('href')).offset().top - 70;
 
-  $('#accordion-js .header').on('click', function() {
-    if ($(this).parent().find('.collapse-container').hasClass('collapsed')) {
-      console.log("yes");
-      $('#accordion-js .collapse-container').removeClass('collapsed');
-    } else {
-      $('#accordion-js .collapse-container').removeClass('collapsed');
-      $(this).parent().find('.collapse-container').addClass('collapsed');
-    }
-  });
+      $('html, body').animate({
+        scrollTop: scrollTo
+      }, 300);
+    });
+  },
 
-  // Navbars
-  //
-  $('.navbar-toggler').on('click', function() {
-    $(this).find('i').toggleClass('close-icon');
-    $(this).parent().find('nav').toggleClass('show-menu');
-  });
-});
+  collapse: function() {
+    $('#basic-collapse > button').on('click', function () {
+      $('#basic-collapse > .collapse-container').toggleClass('collapsed');
+    });
+
+    $('#accordion-js .header').on('click', function () {
+      if ($(this).parent().find('.collapse-container').hasClass('collapsed')) {
+        console.log("yes");
+        $('#accordion-js .collapse-container').removeClass('collapsed');
+      } else {
+        $('#accordion-js .collapse-container').removeClass('collapsed');
+        $(this).parent().find('.collapse-container').addClass('collapsed');
+      }
+    });
+  },
+
+  dropDowns: function() {
+    $('.toggle').on('click', function () {
+      $(this).parent().toggleClass('dropped');
+    });
+  },
+
+  modals: function() {
+    var self = this;
+    var scrollbarWidth = self._getScrollbarWidth();
+    var overflowing = self._isOverflowing();
+
+    // Ensures that scrollbarWidth do not come with decimals
+    scrollbarWidth = scrollbarWidth > 1 ? scrollbarWidth : '';
+
+    $(window).on('resize', function () {
+      overflowing = self._isOverflowing();
+    });
+
+    $('.modal-launcher').on('click', function () {
+      var modalTarget = $(this).data('target');
+      $(modalTarget).addClass('active');
+      $('html').addClass('clipped').css({
+        'padding-right': overflowing ? scrollbarWidth : ''
+      });
+    });
+    $('.modal, .modal-center, .modal .close, .modal-center .close').on('click', function () {
+      $('.modal, .modal-center').removeClass('active');
+      setTimeout(function () {
+        $('html').removeAttr('style');
+        $('html').removeClass('clipped');
+      }, 200);
+    });
+    $('.modal-content').on('click', function (e) {
+      e.stopPropagation();
+    });
+  },
+
+  navbars: function() {
+    $('.navbar-toggler').on('click', function () {
+      $(this).find('i').toggleClass('close-icon');
+      $(this).parent().find('nav').toggleClass('show-menu');
+    });
+  },
+
+  _getScrollbarWidth: function() {
+    var $html = $('html')[0];
+    return window.innerWidth - $html.getBoundingClientRect().width;
+  },
+
+  _isOverflowing: function() {
+    var rect = $('body')[0].getBoundingClientRect();
+    return rect.width < window.innerWidth;
+  }
+}
+
+$(document).ready(Cactu.init());
